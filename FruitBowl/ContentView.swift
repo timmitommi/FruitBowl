47b15b301b0cd058401336fbf153739f1319ecb3
Tompa <timaslj@gmail.com>
2023-06-07 21:41:31 +0200

    Initial commits (#1)
    * Inital commit
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: Timas Ljungdahl <timas.ljungdahl@bontouch.com>

---
+++ FruitBowl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruitBowl
+//
+//  Created by timas on 2023-06-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitBowl/FruitBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitBowlApp.swift
+//  FruitBowl
+//
+//  Created by timas on 2023-06-07.
+//
+
+import SwiftUI
+
+@main
+struct FruitBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
